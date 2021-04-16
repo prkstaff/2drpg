8c4ce052571ede5bae0fa7cae813a519ed5e59c7
Renato Prado <prkstaff@gmail.com>
2021-04-15 22:43:28 -0300

    using renderer in draw method

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"github.com/prkstaff/2drpg/scenes"
 	"github.com/prkstaff/2drpg/settings"
 	"github.com/veandco/go-sdl2/sdl"
+	"os"
 )
 
 // embeddedFS holds our game assets so we can distribute our game as a single binary
@@ -16,8 +18,8 @@ type Game struct {
 	scenes       map[string]scenes.Scene
 	currentScene string
 	Window *sdl.Window
-	Surface *sdl.Surface
 	windowName string
+	renderer *sdl.Renderer
 }
 
 func (g Game) run()  {
@@ -33,16 +35,13 @@ func (g Game) run()  {
 		panic(err)
 	}
 	defer g.Window.Destroy()
-
-	g.Surface, err = g.Window.GetSurface()
+	g.renderer , err = sdl.CreateRenderer(g.Window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
+		os.Exit(2)
 	}
-	g.Surface.FillRect(nil, 0)
+	defer g.renderer.Destroy()
 
-	rect := sdl.Rect{0, 0, 200, 200}
-	g.Surface.FillRect(&rect, 0xffff0000)
-	g.Window.UpdateSurface()
 	running := true
 	for running {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
@@ -65,15 +64,49 @@ func (g *Game) loadScene(sceneName string) {
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
-func (g *Game) Update() error {
+func (g *Game) Update() {
 	// Write your game's logical update.
 	g.scenes[g.currentScene].Update()
-	return nil
 }
 
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw() {
+	var points []sdl.Point
+	var rect sdl.Rect
+	var rects []sdl.Rect
+	renderer := g.renderer
+	renderer.SetDrawColor(0, 0, 0, 255)
+	renderer.Clear()
+
+	renderer.SetDrawColor(255, 255, 255, 255)
+	renderer.DrawPoint(150, 300)
+
+	renderer.SetDrawColor(0, 0, 255, 255)
+	renderer.DrawLine(0, 0, 200, 200)
+
+	points = []sdl.Point{{0, 0}, {100, 300}, {100, 300}, {200, 0}}
+	renderer.SetDrawColor(255, 255, 0, 255)
+	renderer.DrawLines(points)
+
+	rect = sdl.Rect{300, 0, 200, 200}
+	renderer.SetDrawColor(255, 0, 0, 255)
+	renderer.DrawRect(&rect)
+
+	rects = []sdl.Rect{{400, 400, 100, 100}, {550, 350, 200, 200}}
+	renderer.SetDrawColor(0, 255, 255, 255)
+	renderer.DrawRects(rects)
+
+	rect = sdl.Rect{250, 250, 200, 200}
+	renderer.SetDrawColor(0, 255, 0, 255)
+	renderer.FillRect(&rect)
+
+	rects = []sdl.Rect{{500, 300, 100, 100}, {200, 300, 200, 200}}
+	renderer.SetDrawColor(255, 0, 255, 255)
+	renderer.FillRects(rects)
+
+	renderer.Present()
+	sdl.Delay(16)
 	// Write your game's rendering.
 
 	//g.scenes[g.currentScene].Draw(screen)
